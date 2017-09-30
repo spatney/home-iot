0855e777565bc933e4408c3c3eeb1d417070c444
Sachin Patney <spatney@microsoft.com>
2017-09-30 13:39:28 -0700

    refactor

--- index.js
+++ index.js
@@ -1,57 +1,17 @@
 const SpiderSocket = require('spider-device');
-
-const Light = require('./lib/light');
-const Garage = require('./lib/garage');
-const Servo = require('./lib/servo');
-
-
-const port = 3000;
-const light = new Light(1);
-const garage = new Garage(3);
+const DeviceController = require('./lib/deviceController');
 
 const socket = new SpiderSocket({
     appId: '123456',
     uid: 'home'
 });
 
+const deviceController = new DeviceController(socket);
+
 socket.register(() => {
     console.log('registered');
     socket.on('command', command => {
         console.log('running command');
-        execCommand(command);
+        deviceController.execCommand(command);
     });
 });
-
-function execCommand(command) {
-    switch (command.device) {
-        case 'light':
-            let action = command.action;
-            light.state(action);
-            break;
-        case 'garage':
-            garage.toggle();
-            break;
-
-        case 'servo':
-            let id = command.id;
-            let angle = command.angle;
-
-            new Servo(id)
-                .turn(angle)
-                .then(() => {
-                    res.json({
-                        id: id,
-                        angle: angle
-                    });
-                })
-                .catch((e) => {
-                    res.status(500).json({
-                        id: id,
-                        angle: angle,
-                        error: e
-                    });
-                });
-            break;
-        default: console.error('command unknown', JSON.stringify(command));
-    }
-}
